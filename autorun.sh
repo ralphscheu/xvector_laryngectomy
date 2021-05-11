d5836bfcabeb14934aa8a4c8f7231c5dd9e446a9
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2021-05-11 17:35:53 +0200

    refactor autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -3,54 +3,16 @@
 . ./path.sh
 set -e
 
-stage=7
+start_stage=7
 cuda_device_id=0
 
 . ./utils/parse_options.sh
 
 
-if [ $stage -le 0 ]; then
-  ./run.sh --stage 0
-fi
-
-if [ $stage -le 1 ]; then
-  ./run.sh --stage 1
-fi
-
-if [ $stage -le 2 ]; then
-  ./run.sh --stage 2
-fi
-
-if [ $stage -le 3 ]; then
-  ./run.sh --stage 3
-fi
-
-if [ $stage -le 4 ]; then
-  ./run.sh --stage 4
-fi
-
-if [ $stage -le 5 ]; then
-  ./run.sh --stage 5
-fi
-
-if [ $stage -le 6 ]; then
-  ./run.sh --stage 6
-fi
-
-if [ $stage -le 7 ]; then
-  ./run.sh --stage 7 --cuda_device_id $cuda_device_id
-fi
-
-if [ $stage -le 8 ]; then
-  ./run.sh --stage 8 --cuda_device_id $cuda_device_id
-fi
-
-if [ $stage -le 9 ]; then
-  ./run.sh --stage 9
-fi
-
-if [ $stage -le 10 ]; then
-  ./run.sh --stage 10
-fi
+for run_stage in 1 2 3 4 5 6 7 8 9 10 11 12; do
+  if [ $start_stage -le $run_stage ]; then
+    ./run.sh --stage $run_stage
+  fi
+done
 
 exit 0;
