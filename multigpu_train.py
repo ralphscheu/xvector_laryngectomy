5ec1339e923ab394b1ee94808b428ff576fe9705
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2021-05-26 11:36:01 +0200

    speed up multigpu_train

--- multigpu_train.py
+++ multigpu_train.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 from torch.nn import functional as F
 import torch.multiprocessing as mp
 from torch.nn.parallel import DistributedDataParallel as DDP
-from local.torch_xvector.train_utils import nnet3EgsDLNonIterable
+from local.torch_xvector.train_utils import nnet3EgsDLNonIterable, nnet3EgsDL
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import DataLoader
 import torchvision
@@ -17,12 +17,14 @@ from local.torch_xvector.models import *
 from local.torch_xvector.train_utils import getParams
 
 
-def setup(rank, world_size, batch_size, args):
-    dist.init_process_group(backend='nccl', init_method='env://', rank=rank, world_size=world_size)
-    model = xvecTDNN(numSpkrs=7323, p_dropout=0, rank=rank).to(rank)
+def setup(rank, args):
+    dist.init_process_group(backend='nccl', init_method='env://', rank=rank, world_size=args.world_size)
+    torch.backends.cudnn.benchmark = True
+
+    model = xvecTDNN(numSpkrs=args.numSpkrs, p_dropout=0, rank=rank).to(rank)
     model = DDP(model, device_ids=[rank])
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=args.baseLR)
+    optimizer = optim.AdamW(model.parameters(), lr=args.baseLR)
     numBatchesPerArk = int(args.numEgsPerArk/args.batchSize)
     total_steps = args.numEpochs * args.numArchives
     lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,
@@ -59,33 +61,33 @@ def update_dropout(model, step, total_steps, args):
             x.p = p_drop
     return p_drop
 
-def get_egs_dataloader(egs_filepath, batch_size, world_size, rank):
+def get_dist_dataloader(egs_filepath, args, rank):
     train_dataset = nnet3EgsDLNonIterable(egs_filepath)
-    train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank)
-    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=False, num_workers=0, pin_memory=True, sampler=train_sampler)
+    train_sampler = DistributedSampler(train_dataset, num_replicas=args.world_size, rank=rank)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=args.batchSize, shuffle=False, num_workers=0, pin_memory=True, sampler=train_sampler)
     return train_loader
 
 def cleanup():
     dist.destroy_process_group()
 
-def train(rank, world_size, batch_size, args):
+def train(rank, args):
     if rank == 0:
         checkpoint_dir = './models/{}_event_{}'.format("xvector", datetime.now().strftime('%Y%m%dT%H%M%S'))
         os.makedirs(checkpoint_dir)
     
-    model, loss_fn, optimizer, lr_scheduler = setup(rank, world_size, batch_size, args)
+    model, loss_fn, optimizer, lr_scheduler = setup(rank, args)
     
     step = 0
-    logging_loss = 0.0
-    # total_steps = args.numEpochs * args.numArchives
-    total_steps = 2
+    # logging_loss = 0.0
+    total_steps = args.numEpochs * args.numArchives
+    #  total_steps = 2
     while step < total_steps:
         starttime_archive = datetime.utcnow()
         archive_id = step % args.numArchives + 1
         if rank == 0:
-            print("\nProcessing archive {} (epoch {})".format( archive_id, int(max(1,math.ceil(step/args.numArchives))) ))
+            print("\nProcessing archive {} (epoch {})".format( archive_id, int(max(1,math.ceil(step/args.numArchives)))))
 
-        dataloader = get_egs_dataloader("{}/egs.{}.ark".format(args.featDir, archive_id), batch_size, world_size, rank)
+        dataloader = get_dist_dataloader("{}/egs.{}.ark".format(args.featDir, archive_id), args, rank)
         current_batch = 0
         for _, (X,y) in dataloader:
             X = X['matrix'].to(rank)
@@ -98,28 +100,28 @@ def train(rank, world_size, batch_size, args):
             optimizer.step()
             lr_scheduler.step()  # Update Learning Rate
 
-            logging_loss += loss.item()
+            # logging_loss += loss.item()
             current_batch += 1
 
         if rank == 0:
-            save_checkpoint(step, archive_id, model, optimizer, loss, checkpoint_dir, args)
             print('Archive processing time: {}'.format(datetime.utcnow() - starttime_archive))
-            print('Avg Loss/batch: %1.3f' % (logging_loss / int(args.numEgsPerArk/args.batchSize)))
+            save_checkpoint(step, archive_id, model, optimizer, loss, checkpoint_dir, args)
+            # print('Avg Loss/batch: %1.3f' % (logging_loss / int(args.numEgsPerArk/args.batchSize)))
 
         p_drop = update_dropout(model, step, total_steps, args)
         if rank == 0:
             print('Dropout updated to {}'.format(p_drop))
 
-        logging_loss = 0.0
+        # logging_loss = 0.0
         step += 1
 
     cleanup()
 
 if __name__ == "__main__":
     os.environ['MASTER_ADDR'], os.environ['MASTER_PORT'] = 'localhost', '12355'
-    num_gpus = len(os.environ["CUDA_VISIBLE_DEVICES"].split(','))
     parser = getParams()
     args = parser.parse_args()
+    args.world_size = len(os.environ["CUDA_VISIBLE_DEVICES"].split(','))
     torch.manual_seed(0)
     np.random.seed(0)
-    mp.spawn(train, args=(num_gpus, 64, args), nprocs=num_gpus, join=True)
+    mp.spawn(train, args=(args,), nprocs=args.world_size, join=True)
--- multigpu_train.sh
+++ multigpu_train.sh
@@ -5,14 +5,20 @@ set -e
 echo "$0 $@"  # Print the command line for logging.
 
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 \
+CUDA_VISIBLE_DEVICES=6,7 \
 $train_cmd logs/train_xvector_4gpu.log \
-    python3 multigpu_train.py exp/torch_xvector_1a/egs
+    python3 multigpu_train.py \
+        --batchSize 32 \
+        exp/torch_xvector_1a/egs
 
 # CUDA_VISIBLE_DEVICES=4,5 \
 # $train_cmd logs/train_xvector_2gpu.log \
-#     python3 multigpu_train.py exp/torch_xvector_1a/egs
+#     python3 multigpu_train.py \
+#         --batchSize 64 \
+#         exp/torch_xvector_1a/egs
 
-# CUDA_VISIBLE_DEVICES=3 \
+# CUDA_VISIBLE_DEVICES=4 \
 # $train_cmd logs/train_xvector_1gpu.log \
-#     python3 multigpu_train.py
+#     python3 multigpu_train.py \
+#         --batchSize 64 \
+#         exp/torch_xvector_1a/egs
