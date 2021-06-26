da0aedb8de643512a2fff6832cc22bb300770345
scheuererra68323 <scheuererra68323@th-nuernberg.de>
2021-06-26 11:54:01 +0200

    record training loss for evaluation

--- local/predict_scoring/run_regression.py
+++ local/predict_scoring/run_regression.py
@@ -16,7 +16,7 @@ from datetime import datetime
 import torch
 from torch.nn import functional as F
 
-RESULTS_COLUMNS = ['model', 'num_epochs', 'lr', 'crit', 'speaker_group', 'avg_mse', 'avg_mae', 'pearson_r', 'p_value', 'r2']
+RESULTS_COLUMNS = ['model', 'num_epochs', 'lr', 'crit', 'speaker_group', 'avg_mse', 'avg_mae', 'pearson_r', 'p_value', 'r2', 'train_loss_folds']
 SEL_GROUP_COMBINATIONS = [['LARY'], ['PARE'], ['LARY', 'PARE'], ['CTRL']]
 
 
@@ -63,7 +63,7 @@ def run_pytorch_model(title, df, model, criterion=torch.nn.L1Loss(), lr=1e-5, nu
 
             y_preds = []
             y_tests = []
-            fold_results = {'mse': {}, 'mae': {}}
+            fold_results = {'mse': {}, 'mae': {}, 'train_loss': {}}
             for fold, (train_ids, test_ids) in enumerate( LeaveOneOut().split(X) ):
                 # generate train and test portion, scale X_train, X_test according to X_train mean+std
                 xscaler = StandardScaler()
@@ -74,12 +74,15 @@ def run_pytorch_model(title, df, model, criterion=torch.nn.L1Loss(), lr=1e-5, nu
 
                 optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
+                train_loss = []
                 # Run the training loop for defined number of epochs
                 for epoch in range(0, num_epochs):
                     loss = criterion(model(X_train), y_train)
                     loss.backward()
                     optimizer.step()
                     optimizer.zero_grad()
+
+                    train_loss.append(loss.item())
 
                 # evaluate on test
                 model.eval()
@@ -94,6 +97,8 @@ def run_pytorch_model(title, df, model, criterion=torch.nn.L1Loss(), lr=1e-5, nu
                 
                 fold_results['mse'][fold] = mse
                 fold_results['mae'][fold] = mae
+
+                fold_results['train_loss'][fold] = train_loss
             
             y_preds = np.concatenate(y_preds)
             y_tests = np.concatenate(y_tests)
@@ -118,7 +123,8 @@ def run_pytorch_model(title, df, model, criterion=torch.nn.L1Loss(), lr=1e-5, nu
                 'crit': crit, 'speaker_group': sel_speaker_groups, 
                 'avg_mse': sum_mse / len(fold_results['mse'].items()), 
                 'avg_mae': sum_mae / len(fold_results['mae'].items()), 
-                'pearson_r': r, 'p_value': pvalue, 'r2': r2
+                'pearson_r': r, 'p_value': pvalue, 'r2': r2,
+                'train_loss_folds': fold_results['train_loss']
                 }, ignore_index=True)
 
             if print_results:
@@ -250,7 +256,7 @@ if __name__ == "__main__":
 
     ### PyTorch models
 
-    for _num_epochs in [200, 500, 1000]:
+    for _num_epochs in [200, 500]:
         for lr in [1e-3, 1e-4]:
 
             all_results = all_results.append( run_pytorch_model(
@@ -265,7 +271,7 @@ if __name__ == "__main__":
     
 
     # save results
-    filename = 'xvectors/{}/predict_scoring_results__{}.pkl'.format(args.nnet_name, datetime.utcnow().strftime(r'%Y%m%dT%H%M%s'))
+    filename = 'xvectors/{}/predict_scoring_results__{}.pkl'.format(args.nnet_name, datetime.utcnow().strftime(r'%Y%m%dT%H%M%S'))
     print(f"Saving results to {filename}...")
     all_results.to_pickle(filename)
     print("Done.")
